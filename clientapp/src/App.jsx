d12dd131e44b66dbc49ecb20dc622280a8410984
HaavardJenz1996 <haajensen@hotmail.com>
2025-05-01 16:14:35 +0200

    ClientReact application init

---
+++ clientapp/src/App.jsx
@@ -0,0 +1,15 @@
+import Customers from './Components/Customers';
+
+const App = () => {
+  return (
+    <div>
+      <Customers />
+    </div>
+  );
+};
+
+
+export default App;
+
+
+
---
+++ clientapp/src/Components/Customers.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+const Customers = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/customers')  // relative to the ASP.NET Core server
+      .then(res => res.json())
+      .then(data => setProjects(data))
+      .catch(err => console.error('Error:', err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Projects</h2>
+      <ul>
+        {projects.map(project => (
+          <li key={project.id}>{project.name} - {project.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Customers;
