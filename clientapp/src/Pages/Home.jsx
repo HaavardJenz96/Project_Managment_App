67b6fe393a89a847f94618f0ef9cbdd7b06e9f38
HaavardJenz1996 <haajensen@hotmail.com>
2025-05-03 13:42:12 +0200

    HomePage

--- clientapp/src/Pages/Home.jsx
+++ clientapp/src/Pages/Home.jsx
@@ -1,10 +1,26 @@
 import NavbarCustom from "../Components/Navbar";
+import {Card, Row, Col,Button }from 'react-bootstrap' 
+import GridCustomers from "../Components/GridCustomers";
 
 export default function Home() {
   return (
     <div>
       <NavbarCustom></NavbarCustom>
-      <h1>Home</h1>
+      <h1 className="text-center">Home</h1>
+
+      <Card border="light" className="shadow-sm">
+      <Card.Header>
+        <Row className="align-items-center">
+          <Col>
+            <h5>Account Managers</h5>
+          </Col>
+          <Col className="text-end">
+            <Button variant="secondary" size="sm">See all</Button>
+          </Col>
+        </Row>
+        <GridCustomers/>
+      </Card.Header>
+      </Card>
     </div>
   );
 }
